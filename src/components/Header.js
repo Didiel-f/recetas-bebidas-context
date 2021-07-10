57a59e651dcadb0a865021ac8bf60654f1cf6e78
Didiel Figueroa <didiel.f@gmail.com>
2021-07-10 17:03:05 -0400

    creando formulario

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <header className="bg-alert">
+            <h1>Busca recetas de bebidas</h1>
+        </header>
+    )
+}
